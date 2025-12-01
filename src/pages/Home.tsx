2d4b3b91d44c7d31cfa9420f44b381fb0ced5bff
Charity Chinemerem Okechukwu <okechukwucharitychinemerem@gmail.com>
2025-12-01 08:50:53 +0000

    Fix image file names to match case sensitivity in About and Home components

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -219,14 +219,14 @@ export default function Home() {
 						<p style={bodyStyle}>
 							Meet Mumbi Ndung'u, a renowned development specialist and social entrepreneur, recognized for her leadership in digital development.</p>
 
-						<a className="link" href="/leaders/amina-sow" style={linkStyle}>
+						<a className="link" href="#" style={linkStyle}>
 							Learn more
 						</a>
 					</article>
 
 					<article className="card" style={cardStyle} aria-labelledby="upcoming-event">
 						<div style={mediaSquare}>
-							<img src="/assets/banner.png" alt="Upcoming event illustration" style={{ width: 110, height: 110, objectFit: 'cover', borderRadius: 8 }} />
+							<img src="/assets/Banner.png" alt="Upcoming event illustration" style={{ width: 110, height: 110, objectFit: 'cover', borderRadius: 8 }} />
 						</div>
 
 						<h3 id="upcoming-event" style={titleStyle}>
@@ -245,7 +245,7 @@ export default function Home() {
 
 					<article className="card" style={cardStyle} aria-labelledby="latest-news">
 						<div style={mediaSquare}>
-							<img src="/assets/news.png" alt="Latest news illustration" style={{ width: 110, height: 110, objectFit: 'cover', borderRadius: 8 }} />
+							<img src="/assets/News.png" alt="Latest news illustration" style={{ width: 110, height: 110, objectFit: 'cover', borderRadius: 8 }} />
 						</div>
 
 						<h3 id="latest-news" style={titleStyle}>
